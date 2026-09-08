1d95264fd478841323ba6c170c6809fcf39280b5
Maaz K <khokharmaaz@gmail.com>
2026-09-08 17:53:48 +0000

    completed guessing game, flags are -v and --debig

--- guessing.c
+++ guessing.c
@@ -2,27 +2,56 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
+#include <string.h>
 
-#define DEBUG true;
+bool debug = false;
+bool verbose = false;
 
 int random();
 
-int main(void)
+int main(int argc, char *argv[])
 {
     int winning_number = random();
     int guess;
+    int tries = 0;
+
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], "--debug") == 0) {
+            debug = true;
+        }
+
+        if (strcmp(argv[i], "-v") == 0) {
+            verbose = true;
+        }
+    }
+
+    if (debug == true) {
+        printf("DEBUG: number is %d\n", winning_number);
+    }
 
     printf("Guessing game!\n");
     printf("Pick the right number!\n");
+    
+    do {
+        printf("What is your pick: ");
+        scanf("%d", &guess);
 
-    if (DEBUG)
-    {
-    }
+        printf("You said: %d\n", guess);
 
-    while (guess != winning_number)
-    {
-        printf("");
-        printf("");
+
+        if (guess < winning_number) {
+            printf("More...");
+        } else if (guess > winning_number) {
+            printf("Less...");
+        }
+
+        tries++;
+    } while (guess != winning_number);
+
+    printf("You guessed correctly!\n");
+    
+    if (verbose == true) {
+        printf("it took you %d tries\n", tries);
     }
 
     return 0;
